d4674aa68ec984a4cc93fc73deebff2d4894711f
Oleg Genin <oleggenin@ya.ru>
2026-01-02 19:27:27 +0300

    setting.py (flake8)

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 DEBUG = True
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['localhost', '158.160.91.130', 'taski2025og.hopto.org'] 
+ALLOWED_HOSTS = ['localhost', '158.160.91.130', 'taski2025og.hopto.org']
 
 # Application definition
 
